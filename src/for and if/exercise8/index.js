5e6134859cce593d87b688db3a475711d6e3f6be
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-22 14:48:55 +0100

    Exercise8 incoming

--- src/for and if/exercise8/index.js
+++ src/for and if/exercise8/index.js
@@ -1,1 +1,7 @@
-// Put your code here
+for (let outer = 0; outer < 2 ; outer++) {
+    for (let inner = 0; inner < 3; inner++ ){
+        console.log(`Outer: ${outer}, Inner: ${inner}`);
+    }
+}
+
+console.log()

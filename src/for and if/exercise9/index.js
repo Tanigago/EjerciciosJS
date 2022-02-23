cdb4a64b0eb59b5656742d7ba6837cbcfeb4a6d8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 13:35:12 +0100

    Exercise9 Solved

--- src/for and if/exercise9/index.js
+++ src/for and if/exercise9/index.js
@@ -1,7 +1,9 @@
 let numbers = [0,1,2,3,4,5,6,7,8,9]
+const left = [];
+const right = [];
 
 for ( let left of numbers) {
     for (let right of numbers) {
-        // Put your code here
+        console.log(left + "x" + right + " = " + left*right);        
     }
 }

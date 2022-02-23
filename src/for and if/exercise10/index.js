1e9093735035b3a84b4eb50b1bb00ae6cc707ebc
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 14:48:00 +0100

    Exercise10 incoming

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,9 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
+let idx = 0
 
-// Put your code here
+for (let idx = 0; idx < numbers.length ; idx++) {
+    if (idx < idx++) {
+        console.log(numbers)
+    }
+}
+/*Ni idea*/

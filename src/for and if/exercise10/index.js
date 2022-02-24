dac5b43784ec2d096dabfa41c264314dfca40715
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 10:35:27 +0100

    Exercise10 Solved

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,9 +1,21 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
-let idx = 0
+let temporal = null;
 
-for (let idx = 0; idx < numbers.length ; idx++) {
-    if (idx < idx++) {
-        console.log(numbers)
+for (let currentPosition = 0; currentPosition < numbers.length ; currentPosition++) {
+    for (let compareWithPosition = currentPosition+1;
+        compareWithPosition < numbers.length;
+        compareWithPosition++){      
+    
+        if (numbers[currentPosition] > numbers[compareWithPosition]) {
+            temporal = numbers[currentPosition];
+            numbers[currentPosition] = numbers[compareWithPosition];
+            numbers[compareWithPosition] = temporal;
+            console.log(numbers)
+        }
     }
 }
-/*Ni idea*/
+
+
+
+
+/*console.log(`${numbers[currentPosition]} > ${numbers[compareWithPosition]}`)*/

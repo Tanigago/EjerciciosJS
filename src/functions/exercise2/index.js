4a002ff3a2857c107fb7df8cdf631a9ee3ff1bec
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 14:40:04 +0100

    Exercise2 Solved

--- src/functions/exercise2/index.js
+++ src/functions/exercise2/index.js
@@ -1,10 +1,15 @@
 let number = 0;
-
-function plusone(number) {
+/**
+ * 
+ * @param {number} number - Es la variable que ya conocemos. 
+ * @param {number} sumnumber - Es la NUEVA que necesitamos, por eso le tenemos que poner un nombre diferente a number.
+ * @returns {sumnumber} - Solicitamos que nos retorne la nueva variable.
+ */
+function plusone(sumnumber) {
     number = number + 1;
-    return number;
+    return sumnumber;
 }
 
 plusone(number);
 
-console.log(number)
+console.log(number);

b37b781411fd360f36ec0948b16c2e95d91034ea
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 11:29:14 +0100

    Exercise Solved

--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -4,13 +4,24 @@ const numbers3 = [22,6,4,4,5,7,8,9,12,11];
 const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 
 function minorValue(array) {
-
-    // Put your code here
-
+    let mi = array.length;
+    minor = array[mi-1];
+    while (mi--) {
+        if (array[mi] < minor) {
+            minor = array[mi]
+        }
+    }
+    return minor;  
 }
 
-const minor1 = minorValue(numbers1);
-console.log(minor1);
-console.log(minorValue(numbers2));
-console.log(minorValue(numbers3));
-console.log(minorValue(numbers4));
+let a = minorValue(numbers1);
+let b = minorValue(numbers2);
+let c = minorValue(numbers3);
+let d = minorValue(numbers4);
+
+
+
+console.log("El menor valor es:", a)
+console.log("El menor valor es:", b)
+console.log("El menor valor es:", c)
+console.log("El menor valor es:", d)

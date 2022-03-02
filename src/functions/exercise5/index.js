950bee0915b80cd82636ad8a0e18d09ecfeeb2b6
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 14:47:41 +0100

    Exercise5 incoming...

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,6 +1,18 @@
 const numbers = [11,9,13,12]
 
-// Put your code here
+function minorInRange ( principio , final , numbers){
+    let principio = numbers[principio];
+    let final = numbers[final];
+    
+    let mi = array.length;
+    minor = array[mi-1];
+    while (mi--) {
+        if (array[mi] < minor) {
+            minor = array[mi]
+        }
+    }
+    return minor;
+}
 
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));

e6d15eaaeca45e0b0490589304a144778c06f486
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 13:04:33 +0100

    Exercise5 incoming

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -7,9 +7,9 @@ console.log(minorInRange(2,3,numbers));
 console.log(minorInRange(1,3),numbers);
 */
 
-const numbers1 = [11,9,13,12];
+const numbers = [11,9,13,12];
 
-function minorValue(array) {
+function minorInRange(array) {
     let mi = array.length;
     minor = array[mi-1];
     while (mi--) {
@@ -20,6 +20,6 @@ function minorValue(array) {
     return minor;  
 }
 
-let a = minorValue(numbers1);
+let a = minorInRange((0,3),numbers);
 
 console.log("El menor valor es:", a)

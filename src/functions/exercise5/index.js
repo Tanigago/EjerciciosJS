ee6f0ad0ebd26c40a47065dc8b04e94d6dcd6162
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 14:33:58 +0100

    Exercise5 incoming

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,25 +1,7 @@
-/*const numbers = [11,9,13,12]
+const numbers = [11,9,13,12]
 
 // Put your code here
 
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));
 console.log(minorInRange(1,3),numbers);
-*/
-
-const numbers = [11,9,13,12];
-
-function minorInRange(array) {
-    let mi = array.length;
-    minor = array[mi-1];
-    while (mi--) {
-        if (array[mi] < minor) {
-            minor = array[mi]
-        }
-    }
-    return minor;  
-}
-
-let a = minorInRange((0,3),numbers);
-
-console.log("El menor valor es:", a)

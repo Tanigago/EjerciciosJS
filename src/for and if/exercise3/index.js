3e0e14187587a56e6a54f2cc2cd11ef95bfea235
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-03 14:45:31 +0100

    Exercise3 remaked

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -2,9 +2,34 @@ const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
 
-// Put your code here
+for (let item of numbers) {
+    sum = sum + item;
+    average = sum/numbers.length;
+}
 
 console.log(average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

0bb30b4872aacbfb9c2c5eb329cbbdb79d5af52f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-04 13:31:30 +0100

    Exercise10

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,11 +1,17 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
-// Put your code here
 
-
-
-
-
+for (let idx = 0; idx < numbers.length ; idx++) {
+    for (let comparacion = idx+1 ; comparacion < numbers.length ; comparacion++){      
+    
+        if (numbers[idx] > numbers[comparacion]) {
+            temp = numbers[idx];
+            numbers[idx] = numbers[comparacion];
+            numbers[comparacion] = temp;
+            console.log(numbers)
+        }
+    }
+}
 
 
 
@@ -35,15 +41,15 @@ let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 let temporal = null;
 
-for (let currentPosition = 0; currentPosition < numbers.length ; currentPosition++) {
-    for (let compareWithPosition = currentPosition+1;
-        compareWithPosition < numbers.length;
-        compareWithPosition++){      
+for (let idx = 0; idx < numbers.length ; idx++) {
+    for (let comparacion = idx+1;
+        comparacion < numbers.length;
+        comparacion++){      
     
-        if (numbers[currentPosition] > numbers[compareWithPosition]) {
-            temporal = numbers[currentPosition];
-            numbers[currentPosition] = numbers[compareWithPosition];
-            numbers[compareWithPosition] = temporal;
+        if (numbers[idx] > numbers[comparacion]) {
+            temporal = numbers[idx];
+            numbers[idx] = numbers[comparacion];
+            numbers[comparacion] = temporal;
             console.log(numbers)
         }
     }

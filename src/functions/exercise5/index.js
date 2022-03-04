5aaa0e7b87abc4d2d1539500f341bd987485611c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-04 14:48:46 +0100

    Exercise5 interminable...

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,9 +1,38 @@
-const numbers = [11,9,13,12]
+const numbers = [11, 9, 13, 12]
 
-function minorInRange ( principio , final , numbers){
-    let principio = numbers[principio];
-    let final = numbers[final];
+for (let idx = a; idx < b + 1; idx++) {
+    console.log(numbers[idx]);
     
+    function minorInRange(numbers) {
+        let mi = numbers.length;
+        minor = numbers[mi - 1];
+        while (mi--) {
+            if (numbers[mi] < minor) {
+                minor = numbers[mi]
+            }
+        }
+        return minor;
+    }
+}
+
+
+let a = minorInRange(numbers);
+console.log("El menor valor es:", a)
+
+console.log(minorInRange(0, 3, numbers));
+console.log(minorInRange(2, 3, numbers));
+console.log(minorInRange(1, 3), numbers);
+
+/*
+//este for funciona para limitar un array
+for (let idx = 3; idx < 6 ; idx++) {
+    console.log(numbers[idx]);
+}
+
+//esto lo use para buscar el menor numero del array
+const numbers1 = [22,1,4,3,5,7,8,9,12,11];
+
+function minorValue(array) {
     let mi = array.length;
     minor = array[mi-1];
     while (mi--) {
@@ -11,9 +40,9 @@ function minorInRange ( principio , final , numbers){
             minor = array[mi]
         }
     }
-    return minor;
+    return minor;  
 }
 
-console.log(minorInRange(0,3,numbers));
-console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
+let a = minorValue(numbers1);
+console.log("El menor valor es:", a)
+*/

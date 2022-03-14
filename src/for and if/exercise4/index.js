c96be4b1d99ec76672ee2113ed5032ca1d240e54
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 14:48:16 +0100

    Exercise4 remaked

--- src/for and if/exercise4/index.js
+++ src/for and if/exercise4/index.js
@@ -1,4 +1,5 @@
 const numbers = [0,1,2];
+let sum = numbers.length;
 
 for (let item of numbers) {
     let sum = 0;
@@ -37,13 +38,14 @@ console.log(sum)
 
 
 
-
+/*
 const numbers = [0,1,2];
 
 for (let item of numbers) {
     let sum = numbers.length
     console.log(sum);
 }
+*/
 
 
 

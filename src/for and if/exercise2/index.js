4b3d17078f51a6c5bee3aca41d352a5cf984c882
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 14:38:00 +0100

    Exercise2 remaked

--- src/for and if/exercise2/index.js
+++ src/for and if/exercise2/index.js
@@ -1,11 +1,11 @@
 const numbers = [0,1,2,4,5,9,3,6,7,8];
+let counter =  0;
 
-// Put your code here
 for (let item of numbers) {
-    // Put your code here
+    counter = counter+1
 }
 
-console.log(/* Put your code here */)
+console.log(`Hay ${counter} elementos en el array`)
 
 
 

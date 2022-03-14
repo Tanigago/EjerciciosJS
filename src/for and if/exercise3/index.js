a9383a6b5dc1975af1352929ee1385e23ebcc48d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 14:43:46 +0100

    Exercise3 remaked

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -2,9 +2,11 @@ const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
 
-// Put your code here
-
-console.log(average)
+for (let item of numbers) {
+    sum = sum + item;
+}
+    average = sum/numbers.length;
+console.log("El promedio es:", average)
 
 
 

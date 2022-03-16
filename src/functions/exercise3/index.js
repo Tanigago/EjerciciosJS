98beb9d6d4a71bd4fd2f83777567d11f5c754f7b
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-16 14:40:55 +0100

    Exercise3 remaked

--- src/functions/exercise3/index.js
+++ src/functions/exercise3/index.js
@@ -1,42 +1,10 @@
 
-function power (a,b,c){
-    return Math.sqrt???;
+function power (a,b = 2){
+    const total = a**b;
+    return total
 }
 
 console.log(power(25))
 console.log(power(8,1/3))
 console.log(power(2,3))
 console.log(power(5,2))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*SOLUCIONADO
-const number1 = (5**2);
-const number2 = (2**3);
-const number3 = (25**(1/2));
-const number4 = (8**(1/3));
-
-const result1 = Math.sqrt(number1);
-const result2 = Math.sqrt(number2);
-const result3 = Math.sqrt(number3);
-const result4 = Math.sqrt(number4);
-
-console.log(number1)
-console.log(number2)
-console.log(number3)
-console.log(number4)
-*/
